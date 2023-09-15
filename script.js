d0fbf9f906f92983ad22f29761b04ef4e55289c8
Sakshi <sakshiraj713@gmail.com>
2023-09-15 13:39:32 +0530

    Thirteenthchange

---
+++ script.js
@@ -0,0 +1,9 @@
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener("click", function() {
+    const searchTerm = searchInput.value;
+    // Pass the search term to the results page as a URL parameter
+    window.location.href = `results.html?search=${encodeURIComponent(searchTerm)}`;
+});
+
